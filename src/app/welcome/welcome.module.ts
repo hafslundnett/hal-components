886ca38940525e71c6a7df39b20f63c250b54f86
Martin Espeland <m.espeland89@gmail.com>
2019-10-30 15:12:58 +0100

    feat(welcome): added docCards and update dep
    Updated the dependencies,  and added a documentation card to be used in the documentation of available components in HAL.

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome.component';
+import { DocCardModule } from '../shared/doc-card/doc-card.module';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DocCardModule
   ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
