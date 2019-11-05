1262a3c306c5124b47d02ad31148624b837b8d03
Fride Skarseth <fs@computas.com>
2019-11-05 08:31:59 +0100

    Doc(PopupConnectedDoc): More documentation for popup connected

--- src/app/docs-pages/popup-connected-doc/popup-connected-doc.component.ts
+++ src/app/docs-pages/popup-connected-doc/popup-connected-doc.component.ts
@@ -9,20 +9,26 @@ import { ApiTableRow } from 'src/app/shared/models/api-table-row.interface';
 export class PopupConnectedDocComponent implements OnInit {
 
   isOpenEx = false;
+  isOpenEx2 = false;
 
   apiTableRows: ApiTableRow[] = [
     { apiInput: '[isOpen]', description: 'Input for determening wether the popup is open or closed.' },
     { apiInput: '[origin]', description: 'Input for setting origin for what element the popup should connect to.'},
-    { apiInput: '(close)', description: '' },
-    { apiInput: '[relativePositionY]', description: 'Optional input to change the position of the popup in relation to the element its connected to. Options: above, bottom' },
+    { apiInput: '(close)', description: 'Output that closes the popup if the a condition is set to true. The popup will close on default by clicking the close button.' },
+    { apiInput: '[small]', description: 'Input for setting the popups size to small. If true the close button will be removed to allow for more space.'},
+    { apiInput: '[relativePositionY]', description: 'Optional input to change the position of the popup in relation to the element its connected to. Options: above, bottom.' },
   ];
 
+  directiveRows: ApiTableRow[] = [
+    { apiInput: 'CdkOverlayOrigin', description: 'Directive applied to an element to make it usable as an origin for an Overlay.'}
+  ]
+
   htmlCode = `<button type="button" (click)="isOpenEx = !isOpenEx" class="hdd-button" cdkOverlayOrigin #trigger="cdkOverlayOrigin">
-  <span>Click me</span>
+  <span>Open connected popup</span>
 </button>
 <hal-popup-connected [isOpen]="isOpenEx" [origin]="trigger" [relativePositionY]="'below'" (close)="isOpenEx = false">
   <div class="popup-content-wrapper" *ngIf="isOpenEx">
-    <div class="hdd-card">
+    <div class="hdd-card popup-container">
       <div class="hdd-card_header">
         <div class="hdd-card_header_text">
           <div class="hdd-card_title">It works!</div>
