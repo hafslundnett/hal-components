b7cc61bb44e24b4fa433a54b39e682ca365e8c7b
Martin Espeland <m.espeland89@gmail.com>
2019-11-06 13:37:41 +0100

    Feat-started(closable-Row): added doc module for showing the closable row

---
+++ src/app/docs-pages/closable-row-doc/closable-row-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hal-closable-row-doc',
+  templateUrl: './closable-row-doc.component.html',
+  styleUrls: ['./closable-row-doc.component.css']
+})
+export class ClosableRowDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/docs-pages/closable-row-doc/closable-row-doc.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClosableRowDocComponent } from './closable-row-doc.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ClosableRowDocComponent]
+})
+export class ClosableRowDocModule { }
