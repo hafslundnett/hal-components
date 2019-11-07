f85ec55c275917bae5c3acec74e1b670982e128e
Fride Skarseth <fs@computas.com>
2019-11-07 15:01:29 +0100

    test(status-mark): added test for showBlanWhenFalse input

--- projects/hal-components/src/lib/components/status-mark/status-mark.component.spec.ts
+++ projects/hal-components/src/lib/components/status-mark/status-mark.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatusMarkComponent } from './status-mark.component';
 import { By } from '@angular/platform-browser';
 
-describe('StatusMarkComponent', () => {
+fdescribe('StatusMarkComponent', () => {
   let fixture: ComponentFixture<StatusMarkComponent>;
   let component: StatusMarkComponent;
 
@@ -46,4 +46,33 @@ describe('StatusMarkComponent', () => {
       expect(fixture.debugElement.queryAll(By.css('.x-icon')).length).toBe(1);
     });
   });
+
+  describe('setting status to false', () => {
+    beforeEach(() => {
+      component.status = false;
+      fixture.detectChanges();
+    });
+    it('should display a i element with class x-icon, and default mark showBlankWhenFalse should be false', () => {
+      expect(fixture.debugElement.queryAll(By.css('.x-icon')).length).toBe(1);
+    });
+    describe('setting showBlankWhenFalse to true', () => {
+      beforeEach(() => {
+        component.showBlankWhenFalse = true;
+        fixture.detectChanges();
+      });
+      it('does not show x mark', () => {
+        expect(fixture.debugElement.queryAll(By.css('.x-icon')).length).toBe(0);
+      });
+    });
+    describe('setting showBlankWhenFalse to false', () => {
+      beforeEach(() => {
+        component.showBlankWhenFalse = false;
+        fixture.detectChanges();
+      });
+      it('shows x mark', () => {
+        expect(fixture.debugElement.queryAll(By.css('.x-icon')).length).toBe(1);
+      });
+    });
+  });
+
 });
