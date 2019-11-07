f27cd4ec7254ba081beaffb4076af14f909c56a4
Fride Skarseth <fs@computas.com>
2019-11-07 09:13:51 +0100

    Refactor(BoolToText): Renamed to BoolTotEXT

--- projects/hal-components/src/lib/pipes/bool-to-text/bool-to-text.pipe.ts
+++ projects/hal-components/src/lib/pipes/bool-to-text/bool-to-text.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'bool'
+  name: 'boolToText'
 })
 export class BoolToTextPipe implements PipeTransform {
   transform(bool: boolean): string {
