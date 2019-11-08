7332124263c52dfcdc0ef2d81ba34ae57e8c4cdc
Martin Espeland <m.espeland89@gmail.com>
2019-11-08 14:45:14 +0100

    Test(Resizer): Started to add testing for vertical resizer

--- projects/hal-components/src/lib/components/resizer/resizer.component.spec.ts
+++ projects/hal-components/src/lib/components/resizer/resizer.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResizerComponent } from './resizer.component';
 
-describe('ResizerComponent (for vertical)', () => {
+describe('ResizerComponent (for horizontal)', () => {
   let component: ResizerComponent;
   let fixture: ComponentFixture<ResizerComponent>;
 
@@ -11,6 +11,88 @@ describe('ResizerComponent (for vertical)', () => {
       declarations: [ ResizerComponent ]
     })
     .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ResizerComponent);
+    component = fixture.componentInstance;
+    component.resizeDirection = 'horizontal';
+    component.resizableElement = document.createElement('div');
+    fixture.detectChanges();
+  });
+
+  it('should have a horizontal resizer element', () => {
+    expect(getElement('.resizer-horizontal')).not.toBeNull();
+  });
+
+  it('should have an line element', () => {
+    const container: HTMLDivElement = getElement('.resizer-horizontal');
+    expect(container.querySelector('.line')).not.toBeNull();
+  });
+
+  describe('When resizer is pressed', () => {
+    const clientX = 1234;
+
+    beforeEach(() => {
+      component.isResizing = false;
+      component.startResize(new MouseEvent('mousedown', {
+        clientX
+      }));
+    });
+
+    it('the resizing flag should be set', () => {
+      expect(component.isResizing).toBe(true);
+    });
+
+    xit('the mouse X position should be stored', () => {
+      expect(component.initialCursorPosition).toBe(clientX);
+    });
+  });
+
+  describe('When the mouse is released', () => {
+    beforeEach(() => {
+      component.isResizing = true;
+      component.onMouseUp();
+    });
+
+    it('the resizing flag should be set', () => {
+      expect(component.isResizing).toBe(false);
+    });
+  });
+
+  describe('When resizing has occured', () => {
+    const initialCursorPosition = 1000;
+    const newXPos = 1200;
+    const elementWidth = 500;
+
+    beforeEach(() => {
+      component.isResizing = true;
+      component.elementWidth = elementWidth;
+      component.initialCursorPosition = initialCursorPosition;
+      component.onResize(new MouseEvent('mousemove', {
+        clientX: newXPos
+      }));
+    });
+
+    xit('a new width should be set on the resizable element', () => {
+      expect(component.resizableElement.style.width).toBe('700px');
+    });
+  });
+
+  function getElement(selector: string) {
+    return fixture.debugElement.nativeElement.querySelector(selector);
+  }
+});
+
+describe('ResizerComponent (vertical)', () => {
+  let component: ResizerComponent;
+  let fixture: ComponentFixture<ResizerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ResizerComponent]
+    })
+      .compileComponents();
   }));
 
   beforeEach(() => {
