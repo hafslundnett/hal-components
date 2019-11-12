31b9cc7aadf7c147bd43ee1c7124641b1e8376f9
Martin Espeland <m.espeland89@gmail.com>
2019-11-12 13:47:00 +0100

    Test,Refactor(closeable-row): Added test for clos-row
    + Updated name everywhere because of spelling mistake.
    + Update of component, removed descripton area that were unsused.

--- src/app/docs-pages/closeable-row-doc/closeable-row-doc.component.ts
+++ src/app/docs-pages/closeable-row-doc/closeable-row-doc.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ApiTableRow } from 'src/app/shared/models/api-table-row.interface';
 
 @Component({
-  selector: 'hal-closable-row-doc',
+  selector: 'hal-closeable-row-doc',
   templateUrl: './closable-row-doc.component.html',
   styleUrls: ['./closable-row-doc.component.scss']
 })
-export class ClosableRowDocComponent {
+export class CloseableRowDocComponent {
 
   closeableRowInputs: ApiTableRow[] = [
     // tslint:disable-next-line:max-line-length
@@ -15,13 +15,13 @@ export class ClosableRowDocComponent {
     { apiInput: '[startExpanded]', description: 'Optional input to initiate the row as expanded or not.Default behaviour with no input is to initiate the row as expanded' }
   ];
 
-  htmlCode = `<hal-closable-row [noPadding]="true" [startExpanded]="false">
+  htmlCode = `<hal-closeable-row [noPadding]="true" [startExpanded]="false">
   <ng-container ngProjectAs="RowHeader">
     Closebale-Row Header Example
   </ng-container>
   <ng-container ngProjectAs="RowBody">
     The content inside the body of the closeable-row.
   </ng-container>
-</hal-closable-row>`;
+</hal-closeable-row>`;
 
 }
