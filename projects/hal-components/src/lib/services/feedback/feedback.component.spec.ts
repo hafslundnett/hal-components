4199fcad1c9fba2bc76ea508073727fc45793ed2
Martin Espeland <m.espeland89@gmail.com>
2019-11-13 12:55:46 +0100

    Test(Feedback): added test back again

--- projects/hal-components/src/lib/services/feedback/feedback.component.spec.ts
+++ projects/hal-components/src/lib/services/feedback/feedback.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeedbackComponent } from './feedback.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-xdescribe('FeedbackComponent', () => {
+fdescribe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
 
@@ -21,112 +21,111 @@ xdescribe('FeedbackComponent', () => {
     fixture.detectChanges();
   });
 
-  //  Fails in build
-  //
-  // describe('When a message is provided', () => {
-  //   const message = 'Foo bar baz';
+  describe('When a message is provided', () => {
+    const message = 'Foo bar baz';
 
-  //   beforeEach(() => {
-  //     component.message = message;
-  //     fixture.detectChanges();
-  //   });
+    beforeEach(() => {
+      component.message = message;
+      fixture.detectChanges();
+    });
 
-  //   it('the message should be shown', () => {
-  //     const messageElement: HTMLParagraphElement = getElement('.message');
-  //     expect(messageElement.innerText).toBe(message);
-  //   });
-  // });
+    it('the message should be shown', () => {
+      const messageElement: HTMLParagraphElement = getElement('.message');
+      expect(messageElement.innerText).toBe(message);
+    });
+  });
 
-  // describe('When the closable flag is enabled', () => {
-  //   beforeEach(() => {
-  //     component.extras = {
-  //       closable: true,
-  //       duration: 10
-  //     };
-  //     fixture.detectChanges();
-  //   });
+  describe('When the closable flag is enabled', () => {
+    beforeEach(() => {
+      component.extras = {
+        closable: true,
+        duration: 10
+      };
+      fixture.detectChanges();
+    });
 
-  //   it('a close button should be visible', () => {
-  //     const closeButton: HTMLButtonElement = getElement('.close-button');
-  //     expect(closeButton).not.toBeNull();
-  //   });
+    it('a close button should be visible', () => {
+      const closeButton: HTMLButtonElement = getElement('.close-button');
+      expect(closeButton).not.toBeNull();
+    });
 
-//   describe('and the button is pressed', () => {
-//     beforeEach(() => {
-//       component.animationState = 'visible';
-//       const closeButton: HTMLButtonElement = getElement('.close-button');
-//       closeButton.click();
-//       fixture.detectChanges();
-//     });
+    describe('and the button is pressed', () => {
+      beforeEach(() => {
+        component.animationState = 'visible';
+        const closeButton: HTMLButtonElement = getElement('.close-button');
+        closeButton.click();
+        fixture.detectChanges();
+      });
 
-//     it('the animation state should change', () => {
-//       expect(component.animationState).toBe('void');
-//     });
-//   });
-// });
+      it('the animation state should change', () => {
+        expect(component.animationState).toBe('void');
+      });
+    });
+  });
 
-// describe('When the closable flag is disabled', () => {
-//   beforeEach(() => {
-//     component.extras = {
-//       closable: false,
-//       duration: 10
-//     };
-//     fixture.detectChanges();
-//   });
+  describe('When the closable flag is disabled', () => {
+    beforeEach(() => {
+      component.extras = {
+        closable: false,
+        duration: 10
+      };
+      fixture.detectChanges();
+    });
 
-//   it('a close button should not be visible', () => {
-//     const closeButton: HTMLButtonElement = getElement('.close-button');
-//     expect(closeButton).toBeNull();
-//   });
-// });
+    it('a close button should not be visible', () => {
+      const closeButton: HTMLButtonElement = getElement('.close-button');
+      expect(closeButton).toBeNull();
+    });
+  });
 
-// Fails in build
-// describe('When the severity is error', () => {
-//   beforeEach(() => {
-//     component.extras = {
-//       severity: 'error',
-//       duration: 10
-//     };
-//     fixture.detectChanges();
-//   });
+  describe('When the severity is error', () => {
+    beforeEach(() => {
+      component.extras = {
+        severity: 'error',
+        duration: 10
+      };
+      fixture.detectChanges();
+    });
 
-//   it('an info icon should be visible', () => {
-//     const iconElement: HTMLElement = getElement('.fa-info-circle');
-//     expect(iconElement).not.toBeNull();
-//   });
-// });
+    it('an info icon should be visible', () => {
+      const iconElement: HTMLElement = getElement('.fa-info-circle');
+      expect(iconElement).not.toBeNull();
+    });
+  });
 
-// describe('When the severity is info', () => {
-//   beforeEach(() => {
-//     component.extras = {
-//       severity: 'primary',
-//       duration: 10
-//     };
-//     fixture.detectChanges();
-//   });
+  describe('When the severity is info', () => {
+    beforeEach(() => {
+      component.extras = {
+        severity: 'primary',
+        duration: 10
+      };
+      fixture.detectChanges();
+    });
 
-//   it('an info icon should be visible', () => {
-//     const iconElement: HTMLElement = getElement('.fa-info-circle');
-//     expect(iconElement).not.toBeNull();
-// //   });
-// // });
+    it('an info icon should be visible', () => {
+      const iconElement: HTMLElement = getElement('.fa-info-circle');
+      expect(iconElement).not.toBeNull();
+      //   });
+      // });
 
-// describe('When the severity is success', () => {
-//   beforeEach(() => {
-//     component.extras = {
-//       severity: 'success',
-//       duration: 10
-//     };
-//     fixture.detectChanges();
-//   });
+      describe('When the severity is success', () => {
+        beforeEach(() => {
+          component.extras = {
+            severity: 'success',
+            duration: 10
+          };
+          fixture.detectChanges();
+        });
 
-//   it('an check icon should be visible', () => {
-//     const iconElement: HTMLElement = getElement('.fa-check');
-//     expect(iconElement).not.toBeNull();
-//   });
-// });
+        it('an check icon should be visible', () => {
+          const iconElement2: HTMLElement = getElement('.fa-check');
+          expect(iconElement2).not.toBeNull();
+        });
+      });
 
-// function getElement(selector: string) {
-//   return fixture.debugElement.nativeElement.querySelector(selector);
-// }
+    });
+  });
+  function getElement(selector: string) {
+    return fixture.debugElement.nativeElement.querySelector(selector);
+  }
 });
