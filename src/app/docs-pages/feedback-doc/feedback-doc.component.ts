fc6f462807c9a28b74f65d5be30a5b733b9d2b67
Martin Espeland <m.espeland89@gmail.com>
2019-11-14 13:36:10 +0100

    Refactor(Feedback): split documentation table in two, and updated to code highlighter

--- src/app/docs-pages/feedback-doc/feedback-doc.component.ts
+++ src/app/docs-pages/feedback-doc/feedback-doc.component.ts
@@ -9,53 +9,52 @@ import { FeedbackService } from '@hafslundnett/hal-components';
 })
 export class FeedbackDocComponent implements OnInit {
 
-  apiTableRows: ApiTableRow[] = [
-    { apiInput: 'showMessage()', description: 'The main method for generating a feedback message, the arguments are described below:' },
-    { apiInput: '"message"', description: 'The first argument is a string containg the message that appears in the feedback message' },
+  MethodTable: ApiTableRow[] = [
+    { apiInput: 'showMessage()', description: 'The main method for generating a feedback message, the parameters are described below:' },
+  ];
+
+  Parameterstable: ApiTableRow[] = [
     // tslint:disable-next-line:max-line-length
-    { apiInput: 'severity', description: 'The type or severity of the message. Must be set to either : success, primary, error or warn depending on what kind of message you want' },
-    { apiInput: 'closable', description: 'Input for deciding if the message is closable by the user or not. Must be set to true or false' },
-    { apiInput: 'duration', description: 'Input for setting the duration of the message before i closes. Must be defined in miliseconds.' },
+    { apiInput: 'message: string', description: 'The first parameter is a string containg the message that appears in the feedback message' },
+    // tslint:disable-next-line:max-line-length
+    { apiInput: 'severity: string', description: 'The type or severity of the message. Must be set to either : success, primary, error or warn depending on what kind of message you want' },
+    { apiInput: 'closable: boolean', description: 'Deciding if the message is closable by the user or not. Must be set to true or false' },
+    // tslint:disable-next-line:max-line-length
+    { apiInput: 'duration: number', description: 'Setting for the duration of the message before it closes. Must be defined in miliseconds.' },
   ];
 
-  directiveRows: ApiTableRow[] = [
-    { apiInput: 'CdkOverlayOrigin', description: 'Directive applied to an element to make it usable as an origin for an Overlay.' }
-  ];
-
-  isTS = `
-  displaySuccess() {
-    this.feedbackService.showMessage('Great Success!',
+  isTS = `displaySuccess() {
+  this.feedbackService.showMessage('Great Success!',
     {
       severity: 'success',
       closable: true,
       duration: 2000
     });
-  }
-  displayInfo() {
-    this.feedbackService.showMessage('Very informational!',
+}
+displayInfo() {
+  this.feedbackService.showMessage('Very informational!',
     {
       severity: 'info',
       closable: true,
       duration: 2000
     });
-  }
-  displayError() {
-    this.feedbackService.showMessage('Error Error Error!',
+}
+displayError() {
+  this.feedbackService.showMessage('Error Error Error!',
     {
       severity: 'error',
       closable: true,
       duration: 2000
     });
-  }
-  displayWarning() {
+}
+displayWarning() {
   this.feedbackService.showMessage('Warning!',
     {
       severity: 'warn',
       closable: true,
       duration: 2000
     });
-  }
-  `;
+}`;
 
   constructor(private feedbackService: FeedbackService) { }
 
