4078858156b64f2a40042fb0830d1c0b9312331d
Martin Espeland <m.espeland89@gmail.com>
2019-11-18 12:39:00 +0100

    Doc(animations):added examples, needs input.

--- src/app/docs-pages/animations-doc/animations-doc.component.ts
+++ src/app/docs-pages/animations-doc/animations-doc.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { scaleUp, scaleDown, curtainDown, heightDown, listAnimation } from '@hafslundnett/hal-components';
 
 @Component({
   selector: 'hal-animations-doc',
   templateUrl: './animations-doc.component.html',
-  styleUrls: ['./animations-doc.component.scss']
+  styleUrls: ['./animations-doc.component.scss'],
+  animations: [ scaleUp, scaleDown, curtainDown, heightDown, listAnimation ]
 })
-export class AnimationsDocComponent implements OnInit {
+export class AnimationsDocComponent {
+
+  isUpbox = true;
+  isDownbox = true;
+  isCurtain = false;
+  isHeight = false;
+  exampleList: string[] = ['Item' , 'Item' , 'Item'];
 
   constructor() { }
 
-  ngOnInit() {
+  toggleUpBox() {
+    this.isUpbox = !this.isUpbox;
   }
 
+  toggleDownBox() {
+    this.isDownbox = !this.isDownbox;
+  }
+  toggleCurtains() {
+
+    this.isCurtain = !this.isCurtain;
+  }
+  addItem() {
+    if (this.exampleList.length < 10) {
+      this.exampleList.push('Item');
+    }
+  }
+  removeItem() {
+    this.exampleList.pop();
+  }
+
+  toggleHeight() {
+    this.isHeight = !this.isHeight;
+  }
 }
