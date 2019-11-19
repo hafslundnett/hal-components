9f02000dbb29b6d4bedb250b5e60ca5456c90d33
Martin Espeland <m.espeland89@gmail.com>
2019-11-19 14:33:09 +0100

    Doc(Animations): added last doc

--- src/app/docs-pages/animations-doc/animations-doc.component.ts
+++ src/app/docs-pages/animations-doc/animations-doc.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiTableRow } from 'src/app/shared/models/api-table-row.interface';
 import { scaleUp, scaleDown, curtainDown, heightDown, listAnimation } from '@hafslundnett/hal-components';
 
 @Component({
@@ -14,6 +15,37 @@ export class AnimationsDocComponent {
   isCurtain = false;
   isHeight = false;
   exampleList: string[] = ['List Item', 'List Item', 'List Item'];
+
+  AnimationsTable: ApiTableRow[] = [
+    // tslint:disable-next-line:max-line-length
+    { apiInput: '@scaleUp', description: 'Typical used for initiating components. Uses scaling and animates elements to \'pop\' in ' },
+    // tslint:disable-next-line:max-line-length
+    { apiInput: '@scaleDown', description: 'Opposite of @scaleUp, hides the component but does remove them from the dom. Uses scaling down and \'hides\' elements ' },
+    // tslint:disable-next-line:max-line-length
+    { apiInput: '@listAnimation', description: 'Animates lists on initiation based on height, and on list events such as an item being added or removed' },
+    { apiInput: '@curtainDown', description: 'Creates a dropdown effect on elements with scaling' },
+    // tslint:disable-next-line:max-line-length
+    { apiInput: '@heightDown', description: 'Creates a dropdown effect on elements but without scaling, only based on height property of animated element' },
+  ];
+
+  htmlCode = `<!-- @scaleUp animation -->
+<div class="animationBoxes" @scaleUp *ngIf="isUpbox">
+  I'm animated with @scaleUp
+</div>
+
+<!-- @listAnimation  -->
+<ul [@listAnimation]="exampleList.length">
+  <li *ngFor="let item of exampleList"> {{ item }}</li>
+</ul>
+
+<!-- @curtainDown  -->
+<div class="hdd-card" *ngIf="isCurtain" @curtainDown>
+  <div class="hdd-card_header">
+    <div class="hdd-card_header_text">
+      <div class="hdd-card_title">Im a Card</div>
+    </div>
+  </div>
+</div>`;
 
   constructor() { }
 
@@ -37,4 +69,5 @@ export class AnimationsDocComponent {
   toggleHeight() {
     this.isHeight = !this.isHeight;
   }
+
 }
