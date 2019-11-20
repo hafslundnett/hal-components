10402ff89d0291f2ed2177e6be025236a606b5a2
Martin Espeland <m.espeland89@gmail.com>
2019-11-20 16:28:54 +0100

    Refactor(animations): added template comonent to render examples in cards
    + removed unused styles

---
+++ src/app/docs-pages/animations-doc/animations-example/animations-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hal-animations-example',
+  templateUrl: './animations-example.component.html',
+  styleUrls: ['./animations-example.component.scss']
+})
+export class AnimationsExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
