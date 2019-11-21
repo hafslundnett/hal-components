952a2491716b1973629f20ca1fd59351c32b1ca7
Martin Espeland <m.espeland89@gmail.com>
2019-11-21 09:38:25 +0100

    Refactor,Test(loading-spinner): removed input from color and added test + doc

--- projects/hal-components/src/lib/components/loading-spinner/loading-spinner.component.ts
+++ projects/hal-components/src/lib/components/loading-spinner/loading-spinner.component.ts
@@ -8,16 +8,8 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 })
 export class LoadingSpinnerComponent implements OnInit {
 
-  @Input() color = '';
-
   constructor() { }
 
   ngOnInit() {
-    this.loadingSpinnerContainer.nativeElement.style.cssText = this.getCssText();
   }
-
-  private getCssText() {
-    return this.color;
-  }
-
 }
