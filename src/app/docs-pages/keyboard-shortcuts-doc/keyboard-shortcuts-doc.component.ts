2b5e83c5c7810f3dc0d6259cfaa463b2260aa039
Fride Skarseth <fs@computas.com>
2019-11-25 09:19:26 +0100

    docs(keyboard-shortcuts): added doc for keyboard-shortcuts

--- src/app/docs-pages/keyboard-shortcuts-doc/keyboard-shortcuts-doc.component.ts
+++ src/app/docs-pages/keyboard-shortcuts-doc/keyboard-shortcuts-doc.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { KeyboardShortcutsService } from '@hafslundnett/hal-components';
 import { KeyName } from '@hafslundnett/hal-components';
+import { ApiTableRow } from 'src/app/shared/models/api-table-row.interface';
 
 @Component({
   selector: 'hal-keyboard-shortcuts-doc',
@@ -11,6 +12,36 @@ import { KeyName } from '@hafslundnett/hal-components';
 export class KeyboardShortcutsDocComponent implements OnInit, OnDestroy {
 
   private subscriptions: Subscription = new Subscription();
+
+  apiTableRows: ApiTableRow[] = [
+    {
+      apiInput: 'enableKeyboardShortcuts()',
+      description: 'Enables the keyboard-shortcuts. Should be called on application startup'
+    },
+    {
+      apiInput: 'getKeyEventsForKey(keyName, description, requireCtrl, requireShift, requireAlt)',
+      // tslint:disable-next-line:max-line-length
+      description: 'Adds new keyboard-shortcut for the component its called in. The three last values past are booleans for possible keys to press to enable the shortcut-event'
+    }
+  ];
+
+  tsCode = `ngOnInit() {
+  this.keyboardShortcutsService.enableKeyboardShortcuts();
+
+  this.subscriptions.add(
+    this.keyboardShortcutsService.getKeyEventsForKey(
+      KeyName.A,
+      'Send a greeting to the console log!',
+      true,
+      false,
+      false
+    ).subscribe({
+      next: () => {
+        this.greet();
+      }
+    })
+  );
+}`;
 
   constructor(private keyboardShortcutsService: KeyboardShortcutsService) { }
 
