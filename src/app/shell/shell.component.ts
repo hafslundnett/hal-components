011ee768b2e35df37f448e33510f373e6913db13
Martin Espeland <m.espeland89@gmail.com>
2019-11-26 10:14:11 +0100

    Feat(sidenav-title): Initial setup, needs more work, but needs design for active state

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -10,7 +10,7 @@ import { User } from '@hafslundnett/hal-components';
 export class ShellComponent implements OnInit {
 
   menuItems: MenuElement[] = [
-    { url: 'welcome', svgName: 'overview.svg', title: 'Front page', groupTitle: 'HAL' },
+    { url: 'welcome', svgName: 'overview.svg', title: 'Front page', groupTitle: 'HAL', shortTitle: 'HAL' },
     { url: 'hdd', svgName: 'analysis.svg', title: 'HDD', shortTitle: 'HDD' },
   ];
   user: User = { email: 'jan.greger@gmail.com', name: 'Jan Greger Hemb', thumbnail: undefined};
