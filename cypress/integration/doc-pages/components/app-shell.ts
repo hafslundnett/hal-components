b9e278049ab88045f9ef8dd4adc9ea3690a06b90
Martin Espeland <m.espeland89@gmail.com>
2019-11-29 16:08:54 +0100

    changed to typescript

---
+++ cypress/integration/doc-pages/components/app-shell.ts
@@ -0,0 +1,34 @@
+describe('App-Shell Test', () => {
+    it('Visits the App-Shell doc page', () => {
+        cy.visit('http://localhost:4200/app-shell-doc');
+    });
+
+    it('should navigate to welcome page when clicking hafslund log', () => {
+        cy.get('.logo').click();
+        cy.url().should('eq', 'http://localhost:4200/welcome');
+    });
+
+    it('should navigate to hdd section when clicking on hdd logo in sidenav', () => {
+        cy.get('.sidenav').contains('HDD').should('have.attr', 'href', '/hdd').click();
+        cy.url().should('eq', 'http://localhost:4200/hdd');
+    });
+
+    it('should navigate to hal section when clicking on hal logo in sidenav', () => {
+        cy.get('.sidenav').contains('home').should('have.attr', 'href', '/welcome').click();
+        cy.url().should('eq', 'http://localhost:4200/welcome');
+    });
+
+    it('should present a dropdown with navigation to other hafslund apps', () => {
+        cy.get('hal-app-list').click();
+        cy.get('.app-list').should('be.visible');
+        cy.get('body').click(100, 100);
+        cy.get('.app-list').should('be.hidden');
+    });
+
+    it('should present a dropdown with user information', () => {
+        cy.get('.overlay').click();
+        cy.get('.content-wrapper').should('be.visible');
+        cy.get('body').click(100, 100);
+        cy.get('.content-wrapper').should('be.hidden');
+    });
+});
---
+++ cypress/integration/doc-pages/components/pop-up-connected.ts
@@ -0,0 +1,22 @@
+describe('Popup Connected Test', () => {
+    it('Visits the Connected Popup doc page', () =>  {
+        cy.visit('http://localhost:4200/popup-connected-doc');
+    });
+
+    it('should open a popup under button on click(), and dissapear on clicking elsewhere or on close', ()  => {
+        cy.contains('Open connected popup').click();
+        cy.get('.connected-popup-body').should('be.visible');
+        cy.get('.close-button').click();
+        cy.get('.connected-popup-body').should('not.be.visible');
+        cy.contains('Open connected popup').click();
+        cy.get('body').click(100, 100);
+        cy.get('.connected-popup-body').should('not.be.visible');
+    });
+
+    it('should open a popup on top of the button on click, and dissappear on clicking elsewhere in window', ()  => {
+        cy.contains('Open popup on top').click();
+        cy.get('.connected-popup-body').should('be.visible');
+        cy.get('body').click(100, 100);
+        cy.get('.connected-popup-body').should('not.be.visible');
+    });
+});
