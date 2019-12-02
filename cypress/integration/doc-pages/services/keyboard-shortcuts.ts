cd4747c9689898556b8ca07123fe5a93da7cb51b
Martin Espeland <m.espeland89@gmail.com>
2019-12-02 10:49:34 +0100

    Test: Keyboard shortcuts

--- cypress/integration/doc-pages/services/keyboard-shortcuts.ts
+++ cypress/integration/doc-pages/services/keyboard-shortcuts.ts
@@ -1,9 +1,30 @@
 describe('Keyboard shortcut Test', () => {
     it('Visits the Keyboard Shortcuts doc page', () => {
         cy.visit('http://localhost:4200/keyboard-shortcuts-doc');
+    });
 
-        // Doesnt seem that cypress supports keycombinations with space.
-        // cy.get('body').type('{ctrl}{spacebar}')
+    it('should pop up a keyboard shortcut when pressen ctrl and space bar', () => {
+        cy.get('body').type('{ctrl}{ }');
+        cy.get('hal-keyboard-shortcuts-popup').contains('Aktive tastatursnarveier').should('exist');
+        cy.get('body').type('{ctrl}{ }');
+    });
 
+    describe('close keyboard shortcuts popup', () => {
+        beforeEach(() => {
+            cy.get('body').type('{ctrl}{ }');
+        });
+        it('close on double clicking shortcut ', () => {
+            cy.get('body').type('{ctrl}{ }');
+            cy.get('hal-keyboard-shortcuts-popup').should('not.be.visible');
+        });
+        it('close on clicking close button', () => {
+            cy.get('.hdd-card_header_action').click();
+            cy.get('hal-keyboard-shortcuts-popup').should('not.be.visible');
+
+        });
+        it('close on clicking close button', () => {
+            cy.get('body').click(100, 100);
+            cy.get('hal-keyboard-shortcuts-popup').should('not.be.visible');
+        });
     });
 });
