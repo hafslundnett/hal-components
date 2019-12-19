53732dd02e37469bf6deae5718e8a1016c06febf
Fride Skarseth <fs@computas.com>
2019-12-19 09:41:00 +0100

    test(paginator): more cypress tests

--- cypress/integration/doc-pages/components/paginator.ts
+++ cypress/integration/doc-pages/components/paginator.ts
@@ -25,9 +25,7 @@ describe('Paginator Test', () => {
         cy.get('mat-select').click();
         cy.get('mat-option').contains('5').click();
         cy.get('.example-entries').contains('1').should('be.visible');
-        cy.get('.example-entries').contains('2').should('be.visible');
         cy.get('.example-entries').contains('3').should('be.visible');
-        cy.get('.example-entries').contains('4').should('be.visible');
         cy.get('.example-entries').contains('5').should('be.visible');
         cy.get('.example-entries').contains('6').should('not.be.visible');
         cy.get('.example-entries').contains('7').should('not.be.visible');
@@ -54,5 +52,31 @@ describe('Paginator Test', () => {
         cy.get('.page-navigation').contains('3').click();
         cy.get('[name="next-button"]').should('not.be.visible');
         cy.get('[name="previous-button"]').should('be.visible');
+    });
+
+    it('should show ... when more than 5 pages', () => {
+        cy.get('mat-select').click();
+        cy.get('mat-option').contains('2').click();
+        cy.get('[name="next-button"]').click();
+        cy.get('.ellipsis-right').should('be.visible');
+        cy.get('.ellipsis-left').should('not.be.visible');
+        cy.get('.page-navigation').contains('3').click();
+        cy.get('.page-navigation').contains('4').click();
+        cy.get('.ellipsis-right').should('be.visible');
+        cy.get('.ellipsis-left').should('be.visible');
+    });
+
+    it('should set new page on arrow click', () => {
+        cy.get('.page-navigation').contains('1').should('be.visible');
+        cy.get('.page-navigation').contains('3').should('be.visible');
+        cy.get('.page-navigation').contains('5').should('be.visible');
+        cy.get('.page-navigation').contains('13').should('be.visible');
+        cy.get('[name="next-button"]').click();
+        cy.get('[name="next-button"]').click();
+        cy.get('.page-navigation').contains('1').should('be.visible');
+        cy.get('.page-navigation').contains('4').should('not.be.visible');
+        cy.get('.page-navigation').contains('5').should('be.visible');
+        cy.get('.page-navigation').contains('7').should('be.visible');
+        cy.get('.page-navigation').contains('13').should('be.visible');
     });
 });
