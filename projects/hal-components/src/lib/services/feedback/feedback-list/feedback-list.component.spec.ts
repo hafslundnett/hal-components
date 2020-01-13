f8af0a6d9aa57e2da9e2ec8092897de1cdf5405a
Martin Espeland <m.espeland89@gmail.com>
2020-01-13 13:55:17 +0100

    test(feedbacklist): added test for feedback list

--- projects/hal-components/src/lib/services/feedback/feedback-list/feedback-list.component.spec.ts
+++ projects/hal-components/src/lib/services/feedback/feedback-list/feedback-list.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeedbackListComponent } from './feedback-list.component';
+import { FeedbackComponent } from '../feedback.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-xdescribe('FeedbackListComponent', () => {
+describe('FeedbackListComponent', () => {
   let component: FeedbackListComponent;
   let fixture: ComponentFixture<FeedbackListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FeedbackListComponent ]
+      declarations: [FeedbackListComponent, FeedbackComponent],
+      imports: [NoopAnimationsModule]
     })
     .compileComponents();
   }));
@@ -16,10 +18,21 @@ xdescribe('FeedbackListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FeedbackListComponent);
     component = fixture.componentInstance;
+
+    component.feedbackMessageList = [
+      { message: 'Great Success', extras: { severity: 'success', duration: 2000, closable: true}, id: 1 },
+      { message: 'Oh no Dange!', extras: { severity: 'warn', duration: 2000, closable: true}, id: 2 },
+      { message: 'ok, something went wrong', extras: { severity: 'error', duration: 2000, closable: true}, id: 3 },
+    ];
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should exist', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain three elements', () => {
+    const messageList = document.getElementsByTagName('hal-feedback');
+    expect(component.feedbackMessageList.length).toBe(messageList.length);
   });
 });
--- projects/hal-components/src/lib/services/feedback/feedback-list/feedback-list.component.ts
+++ projects/hal-components/src/lib/services/feedback/feedback-list/feedback-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FeedbackElement } from '../feedback-element.interface';
 import { Subject } from 'rxjs';
 
