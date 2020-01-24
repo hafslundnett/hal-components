01822528b73872d97e2076f422f0975a1aef52dc
Jan Greger Hemb <jan.greger@gmail.com>
2020-01-24 09:06:52 +0100

    improvement(shell): improvd shell design

--- projects/hal-components/src/lib/components/app-shell/toolbar/toolbar.module.ts
+++ projects/hal-components/src/lib/components/app-shell/toolbar/toolbar.module.ts
@@ -4,13 +4,15 @@ import { ToolbarComponent } from './toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserMenuModule } from './user-menu/user-menu.module';
 import { SvgElementModule } from '../../svg-element/svg-element.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
     UserMenuModule,
-    SvgElementModule
+    SvgElementModule,
+    RouterModule
   ],
   declarations: [ToolbarComponent],
   exports: [ToolbarComponent]
