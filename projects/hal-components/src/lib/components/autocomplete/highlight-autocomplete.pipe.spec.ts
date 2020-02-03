8753834b677afbe68ccb98e37adb4ea0bee2a6f7
Martin Espeland <m.espeland89@gmail.com>
2020-02-03 12:22:39 +0100

    test(autocomplete): added test for autocomplete Highlighter pipe

--- projects/hal-components/src/lib/components/autocomplete/highlight-autocomplete.pipe.spec.ts
+++ projects/hal-components/src/lib/components/autocomplete/highlight-autocomplete.pipe.spec.ts
@@ -13,4 +13,36 @@ describe('Pipe: HighlightAutocomplete1e', () => {
     const result = pipe.transform(option, searchTerm);
     expect(result).toBe('<b>Elv</b>ia');
   });
+
+  it('returns option without any highlight when searchterm does not match', () => {
+    const option = 'Elvia';
+    const searchTerm = 'elvis';
+    const pipe = new HighlightAutocomplete();
+    const result = pipe.transform(option, searchTerm);
+    expect(result).toBe('Elvia');
+  });
+
+  it('returns all matching inputs with bold tags when search term is matching multiple values in array', () => {
+    const options = ['Elvia', 'Elvis', 'Elves'];
+    const pipe = new HighlightAutocomplete();
+    const searchTerm = 'elv';
+    const results: string[] = [];
+    for (const option of options) {
+      const result = pipe.transform(option, searchTerm);
+      results.push(result);
+    }
+    expect(results).toEqual(['<b>Elv</b>ia', '<b>Elv</b>is', '<b>Elv</b>es']);
+  });
+
+  it('Only return matching options with bold tags, and non matching options without bold tags', () => {
+    const options = ['Elvia', 'Elvis', 'Elves'];
+    const pipe = new HighlightAutocomplete();
+    const searchTerm = 'elvi';
+    const results: string[] = [];
+    for (const option of options) {
+      const result = pipe.transform(option, searchTerm);
+      results.push(result);
+    }
+    expect(results).toEqual(['<b>Elvi</b>a', '<b>Elvi</b>s', 'Elves']);
+  });
 });
