9b9c000522940492b1cee64b4c60db8279fd21cd
JGH153 <jan.greger@gmail.com>
2020-02-10 15:48:13 +0100

    chore(package): updated to angular 9

--- projects/hal-components/src/lib/services/feedback/feedback.service.mock.ts
+++ projects/hal-components/src/lib/services/feedback/feedback.service.mock.ts
@@ -1,5 +1,7 @@
 import { FeedbackExtras } from './feedback-extras';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class FeedbackServiceMock {
   showMessage(message: string, extras?: FeedbackExtras): void { }
 }
